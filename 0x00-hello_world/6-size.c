dc94a55a4d930f4948a6a34512e2ca11e0a6625f
halidujoseph <you@example.com>
2022-09-09 09:51:43 -0700

    size of variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of variables types
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int a;
+	char b;
+	float c;
+	long int d;
+	long long int e;
+
+	printf("Size of int: %zu bytes\n",sizeof(a));
+	printf("Size of char: %zu bytes\n",sizeof(b));
+	printf("Size of float: %zu bytes\n",sizeof(c));
+	printf("Size of long int: %zu bytes\n",sizeof(d));
+	printf("Size of long long int: %zu bytes\n",sizeof(e));
+	return (0);
+}
