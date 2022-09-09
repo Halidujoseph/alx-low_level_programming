b91af2e7aae6d4a4d79be76c13073c3a5baecb14
halidujoseph <you@example.com>
2022-09-09 08:48:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point, print in c  
@@ -7,6 +7,6 @@ include <stdio.h>
  */
 int main(void)
 {
-	put("Programming is like building a multilingual puzzle");
+	put(""Programming is like building a multilingual puzzle");
 	return (0);
 }
