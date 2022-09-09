3619f27699358fcc35949b8d7449f9062cf57ffe
halidujoseph <you@example.com>
2022-09-09 08:44:45 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+
+/**
+ * main - Entry point, print in c  
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle");
+	return (0);
+}
