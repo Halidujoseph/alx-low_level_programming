9b69967a88dff93c5218583b0d581833047e158b
halidujoseph <you@example.com>
2022-09-09 09:55:43 -0700

    sixe of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long int d;
 	long long int e;
 
-	printf("Size of int: %zu bytes\n",sizeof(a));
-	printf("Size of char: %zu bytes\n",sizeof(b));
-	printf("Size of float: %zu bytes\n",sizeof(c));
-	printf("Size of long int: %zu bytes\n",sizeof(d));
-	printf("Size of long long int: %zu bytes\n",sizeof(e));
+	printf("Size of an int: %zu bytes\n", sizeof(a));
+	printf("Size of a char: %zu bytes\n", sizeof(b));
+	printf("Size of a float: %zu bytes\n", sizeof(c));
+	printf("Size of a long int: %zu bytes\n", sizeof(d));
+	printf("Size of a long long int: %zu bytes\n", sizeof(e));
 	return (0);
 }
