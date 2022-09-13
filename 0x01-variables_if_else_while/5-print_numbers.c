991dc89ce48fca8ef7b7eb428e9ccd235c1d5960
halidujoseph <you@example.com>
2022-09-12 18:58:03 -0700

    print numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - remove some alphabets
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
