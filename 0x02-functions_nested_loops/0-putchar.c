7ff57b0e907002cee2886af7c2899c2c76924c5f
halidujoseph <you@example.com>
2022-09-14 16:05:57 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * main - print putchar
  * Return: Always 0
  */
@@ -15,6 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	_putchar('\n');
+	
 	return(0);
 }
