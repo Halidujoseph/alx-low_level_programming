33bbd7c5c5cd4de18c59935ccf6b743c2ae6cdd9
halidujoseph <you@example.com>
2022-09-15 10:21:48 -0700

    multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_to_nagetive(int i);
+int largest_number(int a, int b, int c);
+void print_reminder_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
