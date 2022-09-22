fb4ac29933b9163705029060d15db5bf609cdd1c
halidujoseph <you@example.com>
2022-09-22 12:08:07 -0700

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,12 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-/**
- * void prototypes()
- * int prototypes()
- * char prototypes()
- */
-int _putchar(char c)
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -22,4 +17,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *infinite_add(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 
-#endif /*MAIN_H/
+#endif
