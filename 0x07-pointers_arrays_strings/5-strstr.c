b10a37d2d4f092e5fdf78778f33b7973969c3d91
halidujoseph <you@example.com>
2022-09-28 04:15:04 -0700

    locate a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,21 +10,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *str1, *str2;
+	unsigned int i = 0, j = 0;
 
-	while (*haystack != '\0')
+	while (haystack[i])
 	{
-		str1 = haystack;
-		str2 = needle;
-
-		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			haystack++;
-			str2++;
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
-		if (*str2++ '\0')
-			return (str1);
-		haystack = str1 + 1;
+		if (needle[j])
+		{
+			i++
+				j = 0;
+		}
+		else
+			return (haystack + i);
 	}
 	returm(0);
 }
