33c04ea6a95f9e74e785b74714d41a9000b43490
halidujoseph <you@example.com>
2022-09-28 04:35:47 -0700

    locate a substing

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strstr - locates a substring
+ * _strstr - locate a substring
  * @haystack: string in which to check for needle
  * @needle: substring to find in haystack
  *
  * Return: pointer to beginning of needle in haystack or NULL if no match
- **/
-
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
@@ -23,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 		}
 		if (needle[j])
 		{
-			i++
+			i++;
 			j = 0;
 		}
 		else
