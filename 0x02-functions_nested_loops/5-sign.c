46207874da83551a36181e25259657ccaba07c9d
halidujoseph <you@example.com>
2022-10-01 14:11:32 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,20 +3,22 @@
 /**
  * print_sign - prints the sign of a number
  * @n: input letter
- * Return: Always 0
+ * Return: 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+
 		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-')
-			return (-1);
+		return (-1);
 	}
+
 	_putchar('0');
 	return (0);
 }
