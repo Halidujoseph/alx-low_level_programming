ac9b89943106a8197c8f30d5cc7c69abace7df81
halidujoseph <you@example.com>
2022-10-01 14:03:16 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,22 +3,18 @@
 /**
  * print_sign - prints the sign of a number
  * @n: input letter
- * Return: Always success
+ * Return: Always 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 
 	{
+
 		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
 	{
 		_putchar('-');
 		return (-1);
