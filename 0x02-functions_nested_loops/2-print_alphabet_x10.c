a4fe5d321f09585d7f16d6af7737ea96283bb533
halidujoseph <you@example.com>
2022-09-30 18:14:17 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - function to print alphabet 10 times
+ * print_alphabet_x10 - function to print alphabet 10 times
  *
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int n;
 	
